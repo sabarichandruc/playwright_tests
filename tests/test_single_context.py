7bbfdc348b5b83df8aac5c8ff482cdfdb90c6bf4
SabariChand <csabari95@gmail.com>
2026-01-04 09:24:15 +0530

    workflow

--- tests/test_single_context.py
+++ tests/test_single_context.py
@@ -4,7 +4,6 @@ import pytest
 def test_context(browser):
     context = browser.new_context()
     page1 =context.new_page()
-    page2 =context.new_page()
 
     page1.goto("https://testautomationpractice.blogspot.com/")
 
@@ -12,8 +11,3 @@ def test_context(browser):
 
     page1.wait_for_timeout(5000)
 
-    page2.goto("https://demo.automationtesting.in/Register.html")
-
-    expect(page2).to_have_title("Register")
-
-    page1.wait_for_timeout(1000)
